b99b368b70b8adcbaa5a90a606d83871772657d7
kkshow <kuangft1236@gmail.com>
2014-11-28 22:29:16 +0800

    代码格式化

--- application/library/Ctrl/Base.php
+++ application/library/Ctrl/Base.php
@@ -1,13 +1,15 @@
 <?php
+
 /**
  * 控制器 基础类
  * @author 张洋 2050479@qq.com
  */
-abstract class Ctrl_Base extends Yaf_Controller_Abstract {
+abstract class Ctrl_Base extends Yaf_Controller_Abstract
+{
     /**
      * c层构造方法
      */
-    function init(){
+    function init() {
         /**
          * 如果是Ajax请求, 则关闭HTML输出
          */
@@ -20,7 +22,7 @@ abstract class Ctrl_Base extends Yaf_Controller_Abstract {
     /**
      * 列表
      */
-    public function indexAction(){
+    public function indexAction() {
         #空方法,系统直接调用index.phtml
         #通常调试yaf也在这里啦
     }
@@ -28,27 +30,27 @@ abstract class Ctrl_Base extends Yaf_Controller_Abstract {
     /**
      * 列表取数方法
      */
-    function pageJsonAction(){
+    function pageJsonAction() {
     }
 
     /**
      * 列表取数 - 无分页
      */
-    function listJsonAction(){
+    function listJsonAction() {
     }
 
     /**
      * 增、改
      * @param int $id
      */
-    public function saveAction($id=0){
+    public function saveAction($id = 0) {
     }
 
     /**
      * 删除
      * @param int $id
      */
-    public function delAction($id=0){
+    public function delAction($id = 0) {
     }
 
     /********************** END 列表增删改查 ************************/
